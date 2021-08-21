3a2e20307c8f695adb05607b5ffab3353f8c6d05
andersonarorjdev <anderson.r.o.r.jr@gmail.com>
2021-08-21 19:10:03 -0300

    Front sendo feito

--- app.java
+++ app.java
@@ -2,44 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class app {
+public class app extends menu{
     public static void main(String[] args) throws IOException {
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-        System.out.println("Empresa de importação de produtos LTDA.");
-        System.out.println("Sistema de controle de estoque");
-        System.out.println("Menu Principal");
-
-        System.out.println("1 - Cadastro de Produtos");
-        System.out.println("2 - Movimentacao");
-        System.out.println("3 - Reajustes de Preços");
-        System.out.println("4 - Relatorios");
-        System.out.println("0 - Finalizar");
-
-        String name = reader.readLine();
-       
-       switch (name) {
-           case "1":
-            System.out.println("Cadastro de Produtos");
-            break;
-
-            case "2":
-            System.out.println("Movimentacao");
-            break;
-
-            case "3":
-            System.out.println("Reajustes de Preços");
-            break;
-
-            case "4":
-            System.out.println("Relatorios");
-            break;
-            
-            case "0":
-            System.out.println("Finalizar");
-            break;
-       }
-
+        menu.imprimeMenu();
     }
 }
---
+++ menu.java
@@ -0,0 +1,95 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class menu {
+    
+    public static String imprimeMenu() throws IOException {
+        System.out.println("Empresa de importação de produtos LTDA.");
+        System.out.println("Sistema de controle de estoque");
+        System.out.println("Menu Principal");
+
+        System.out.println("1 - Cadastro de Produtos");
+        System.out.println("2 - Movimentacao");
+        System.out.println("3 - Reajustes de Preços");
+        System.out.println("4 - Relatorios");
+        System.out.println("0 - Finalizar");
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String options = reader.readLine();
+        return options;
+    }    
+
+    //    switch (options) {
+    //        case "1":
+    //         System.out.println("Cadastro de Produtos");
+    //         System.out.println("1 - Inclusão");
+    //         System.out.println("2 - Alteração");
+    //         System.out.println("3 - Consulta");
+    //         System.out.println("0 - Retornar");
+
+    //         opcaoMenu1 = reader.readLine();
+
+    //         switch (opcaoMenu1) {
+    //             case "1":
+    //                 System.out.println("Sistema de Controle de Estoque");
+    //                 System.out.println("Inclusao de Produtos");
+
+    //                 System.out.println("Nome do Produto:");
+    //                 String nomeProduto = reader.readLine();
+
+    //                 System.out.println("Preco Unitario:");
+    //                 String precoUnitario = reader.readLine();
+
+    //                 System.out.println("Quantidade em Estoque:");
+    //                 String quantidadeEstoque = reader.readLine();
+
+    //                 System.out.println("Confirma inclusao em estoque? S ou N");
+    //                 String confirmacao = reader.readLine();
+
+    //                     if (confirmacao.equals("S")) {
+    //                         System.out.println("Produto Incluido com sucesso");
+    //                     } else {
+    //                         System.out.println("Produto não foi incluido");
+    //                     }
+
+    //                     System.out.println("Deseja repetir a operação? S ou N");
+    //                     String  repetir = reader.readLine();
+    //                     if (repetir.equals("S")) {
+    //                         //Vai repetir a operacao
+    //                     }else{
+    //                         //vai voltar par o menu principal
+    //                     }
+
+    //             break;
+
+    //             case "2":
+    //                 System.out.println("Sistema de Controle de Estoque");
+    //                 System.out.println("Alteracao de Produtos");
+
+    //                 System.out.println("Nome do Produto:");
+    //                 String nomeProduto2 = reader.readLine();
+
+    //         }
+
+    //         break;
+
+    //         case "2":
+    //         System.out.println("Movimentacao");
+    //         break;
+
+    //         case "3":
+    //         System.out.println("Reajustes de Preços");
+    //         break;
+
+    //         case "4":
+    //         System.out.println("Relatorios");
+    //         break;
+            
+    //         case "0":
+    //         System.out.println("Finalizar");
+    //         break;
+    //    }
+
+}
