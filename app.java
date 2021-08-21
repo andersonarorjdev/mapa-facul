15e979d2e7d120b985b11351a8220da7bbcf0a5d
andersonarorjdev <anderson.r.o.r.jr@gmail.com>
2021-08-21 17:28:41 -0300

    Adicionado menu para o usuario escolher as opcoes e adicionado descricao na task no Readme

--- app.java
+++ app.java
@@ -5,14 +5,41 @@ import java.io.InputStreamReader;
 public class app {
     public static void main(String[] args) throws IOException {
 
-        // Create a BufferedReader to read the input
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        //Read the data using the readline
-        String name = reader.readLine();
+        System.out.println("Empresa de importação de produtos LTDA.");
+        System.out.println("Sistema de controle de estoque");
+        System.out.println("Menu Principal");
 
-        //Print the readline
-        System.out.println("Hello " + name);
+        System.out.println("1 - Cadastro de Produtos");
+        System.out.println("2 - Movimentacao");
+        System.out.println("3 - Reajustes de Preços");
+        System.out.println("4 - Relatorios");
+        System.out.println("0 - Finalizar");
+
+        String name = reader.readLine();
+       
+       switch (name) {
+           case "1":
+            System.out.println("Cadastro de Produtos");
+            break;
+
+            case "2":
+            System.out.println("Movimentacao");
+            break;
+
+            case "3":
+            System.out.println("Reajustes de Preços");
+            break;
+
+            case "4":
+            System.out.println("Relatorios");
+            break;
+            
+            case "0":
+            System.out.println("Finalizar");
+            break;
+       }
 
     }
 }
