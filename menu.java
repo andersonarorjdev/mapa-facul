3a7b1f97ee3f8b01601613f6579b34c94c6781e4
andersonarorjdev <anderson.r.o.r.jr@gmail.com>
2021-08-21 21:42:56 -0300

    Adicionados novas modificacoes menu

--- menu.java
+++ menu.java
@@ -27,9 +27,10 @@ public class menu {
              System.out.println("3 - Consulta");
              System.out.println("0 - Retornar");
  
-            String opcaoMenu1 = reader.readLine();
+             String opcaoMenu1 = reader.readLine();
  
-                 switch (opcaoMenu1) {
+            switch (opcaoMenu1) {
+
                  case "1":
                      System.out.println("Sistema de Controle de Estoque");
                      System.out.println("Inclusao de Produtos");
@@ -53,6 +54,7 @@ public class menu {
                          }
  
                          System.out.println("Deseja repetir a operação? S ou N");
+
                          String  repetir = reader.readLine();
                          if (repetir.equals("S")) {
                              //Vai repetir a operacao
@@ -67,24 +69,92 @@ public class menu {
                      System.out.println("Alteracao de Produtos");
  
                      System.out.println("Nome do Produto:");
-                     String nomeProduto2 = reader.readLine();
+                     String NomeProduto = reader.readLine();
+                     //Caso o produto nao exista, vai ser criado um novo produto, caso ele exista vai ser alterado, caso nao exista sera imprido que ele nao existe
+                     //Primeiro vai exibir os dados do produto, para poder atualizar.
+
+                     System.out.println("Nome do Produto:");
+                     String nomeAtualizadoProduto = reader.readLine();
  
-             }
+                     System.out.println("Preco Unitario:");
+                     String precoAtualizadoUnitario = reader.readLine();
  
+                     System.out.println("Quantidade em Estoque:");
+                     String quantidadeAtualizadoEstoque = reader.readLine();
+ 
+                     System.out.println("Confirma inclusao em estoque? S ou N");
+                     String confirmacaoAtualizacao = reader.readLine();
+
+
+                break;
+                
+
+                case "3":
+                //Vai ler o nome do produto digitado e verificar se ele existe, caso exista vai mostrar os dados do produto, caso nao exista vai ser imprimido que ele nao existe
+                    System.out.println("Sistema de Controle de Estoque");
+                    System.out.println("Consulta de Produtos");
+
+                    String NomeProdutoConsulta = reader.readLine();
+
+                    boolean ProdutoExiste = false;
+
+                    if(ProdutoExiste){
+                        System.out.println("Produto encontrado");
+                        //Imprime os dados do produto
+                    }else{
+                        System.out.println("Produto nao encontrado");
+                    }
+
+                break;
+
+                case "4":
+                    System.out.println("Retornar menu");
+                break;
+            }
              break;
- 
+            
+
              case "2":
-             System.out.println("Movimentacao");
+                System.out.println("Movimentacao");
+                System.out.println("1 - Entrada");
+                System.out.println("2 - Saída");
+                System.out.println("0 - Retornar");
+
+                String opcaoMenu2 = reader.readLine();
+                while(opcaoMenu2 != "0"){
+                    switch (opcaoMenu2) {
+                        case "1":
+                            System.out.println("Digite o nome do produto á ser buscado");
+                            String NomeProdutoSerBuscado = reader.readLine();
+
+                            //Vai imprimir quantas vezes esse produto foi inserido na estoque(entrada),
+                            //Vai imprimir a quantidade atual desse produto no estoque.
+                            //vai imprimir a quantidade final atual + entrada.
+                            System.out.println("Quantidade de produtos atualmente: ");
+                            System.out.println("Quantidade de produtos na entrada: ");
+                            System.out.println("Quantidade total de produtos:");
+
+                        break;
+
+                        case "2":
+                            System.out.println("Digite o nome do produto á ser buscado");
+                            
+                    }
+                }
+
              break;
- 
+            
+
              case "3":
              System.out.println("Reajustes de Preços");
              break;
- 
+            
+
              case "4":
              System.out.println("Relatorios");
              break;
              
+
              case "0":
              System.out.println("Finalizar");
              break;
